b9b0ab2134039fe7ee22f4d45e882cdf55fcb2db
zarin <zheydari@gmail.com>
2023-12-27 18:23:48 +0330

    add direct link page for clothes course better view and add discount input code

--- resources/views/web/direct-link.blade.php
+++ resources/views/web/direct-link.blade.php
@@ -10,30 +10,6 @@
     <link rel="stylesheet" href="{{ asset('/assets/css/style.css') }}">
     <title>دوره لباس عروسک</title>
     <style>
-        .yummy {
-            color: #fff;
-            padding: 4px 8px 5px;
-            border: 1px solid #9fa3a7;
-            text-decoration: none;
-            /*text-shadow: 0 1px 0 rgba(0,0,0,0.3);*/
-
-            -moz-border-radius: 5px;
-            -webkit-border-radius: 5px;
-            border-radius: 5px;
-            -moz-background-clip: padding;
-            -webkit-background-clip: padding-box;
-            background-clip: padding-box;
-            background-color: #008fcf;
-            box-shadow:
-                    0 1px 0 rgba(0,0,0,.08),
-                    inset 0 1px 2px rgba(255,255,255,.67),
-                    inset 0 -1px 0 rgba(0,0,0,.14);
-            background-image: -moz-linear-gradient(90deg, #a3a9ad 0%, #c5c9cd 100%);
-            background-image: -o-linear-gradient(90deg, #a3a9ad 0%, #c5c9cd 100%);
-            background-image: -webkit-linear-gradient(90deg, #a3a9ad 0%, #c5c9cd 100%);
-            background-image: linear-gradient(90deg, #a3a9ad 0%, #c5c9cd 100%);
-        }
-
         .submit_btn {
             display: flex;
             background: #FBC53B;
@@ -48,43 +24,58 @@
             line-height: 29px;
             text-align: center;
             cursor: pointer;
+            margin: 20px auto 20px; /* Center the button using margin */
+        }
+
+        .discount-code-label {
+            font-size: 13px;
+            margin-right: 12%;
+        }
+
+        body {
+            background-image: linear-gradient(#5f4af1, #d47cea);
+        }
+
+        .direct-link-item {
+            margin: 0 auto;
+            width: 80%;
         }
 
     </style>
 </head>
-<body class="bg-light">
+<body>
 <div class="container">
+    <img src="{{ asset('/assets/images/clothes_course.jpg') }}" width="100%" style="border-radius: 15px;">
 
-    <div class="row px-2">
+    <form method="POST" action="{{ route('direct_link') }}"><!-- start login form -->
+        @csrf
 
-        <div class="col-lg-6 col-md-7 col-10 mx-auto bg-white mt-5 rounded shadow-sm py-3">
+        <div class="input-group mb-4 direct-link-item">
 
-            <form method="POST" action="{{ route('direct_link') }}"><!-- start login form -->
-                @csrf
+            <span class="input-group-text"><i class="fa fa-user"></i></span>
 
-                <div class="input-group signup-form mb-4">
-
-                    <span class="input-group-text"><i class="fa fa-user"></i></span>
-
-                    <input type="text" class="form-control form-control-lg" placeholder="نام و نام خانوادگی" name="name">
-
-                </div>
-
-                <div class="input-group signup-form mb-4">
-
-                    <span class="input-group-text"><i class="fa fa-phone"></i></span>
-                    <input type="text" class="form-control form-control-lg" placeholder="شماره تلفن همراه" name="mobile">
-
-                </div>
-
-{{--                <button type="submit" class="btn btn-lg btn-warning d-block mx-auto px-5 font-18 vazir-font my-3 yummy">ثبت نام و شروع آموزش</button>--}}
-                <button type="submit" class="submit_btn">ثبت نام و شروع آموزش</button>
-
-            </form><!-- end login form -->
+            <input type="text" class="form-control form-control-lg" placeholder="نام و نام خانوادگی" name="name">
 
         </div>
 
-    </div>
+        <div class="input-group mb-1 direct-link-item">
+
+            <span class="input-group-text"><i class="fa fa-phone"></i></span>
+            <input type="text" class="form-control form-control-lg" placeholder="شماره تلفن همراه" name="mobile">
+
+        </div>
+
+        <span class="discount-code-label">اگه کد تخفیف داری بزن 👇👇👇</span>
+        <div class="input-group mb-2 direct-link-item">
+
+            <span class="input-group-text"><i class="fa fa-percent"></i></span>
+            <input type="text" class="form-control form-control-lg" placeholder="کد تخفیف" name="discount_code">
+
+        </div>
+
+        <button type="submit" class="submit_btn">ثبت نام و شروع آموزش</button>
+
+    </form><!-- end login form -->
 
 </div>
 </body>
